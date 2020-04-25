cd0d7346230c750910c82e5f8bbd1cfe3c310346
subhanfadilah <63758109+subhanfadilah@users.noreply.github.com>
2020-04-25 18:39:26 +0700

    Add files via upload

---
+++ sumandingtools.py
@@ -0,0 +1,384 @@
+import os
+import sys
+import time
+
+os.system('clear')
+def jalan(kata):
+	for c in kata:
+		sys.stdout.write(c)
+		sys.stdout.flush()
+		time.sleep(0.1)
+jalan ('[*] Terima Kasih Telah Menggunakan Tools Pertama Saya yang berupa Tools di dalam Tools. hehehe.... :).'.center(10))
+jalan (' Gunakan Tools Ini dengan Bijak Ya Ganss :p.'.center(10))
+jalan (' Pada Dasarnya Semua Tools ini Kemungkinan Berhasil 45-70% Sisanya Tergantung Skill Anda Dalam Menguasai Tools Ini :). [*]'.center(10))
+time.sleep(5)
+os.system('clear')
+os.system('figlet sumandingtools |lolcat')
+print "=" * 46
+print "[+]        Author : Subhan Fadilah         [+]"
+print "[+] Whatsapp : http://wa.me/+6285225218341 [+]"
+print "=" * 46
+time.sleep(5)
+print
+print "[*] Pilihan Tools [*]"
+print
+time.sleep(1)
+print "[1] Hammer"
+time.sleep(1)
+print "[2] Server-Ddos-Website"
+time.sleep(1)
+print "[3] Script Mr.Rffr"
+time.sleep(1)
+print "[4] Perkiraan Cuaca"
+time.sleep(1)
+print "[5] Spam Sms JDJID"
+time.sleep(1)
+print "[6] Game Moon Buggy"
+time.sleep(1)
+print "[7] Hack Fb v1.0"
+time.sleep(1)
+print "[8] Hack Fb v1.2"
+time.sleep(1)
+print "[9] Membuat Script Deface"
+time.sleep(1)
+print "[10] Tools Crazy Sms"
+time.sleep(1)
+print "[11] Rusmana Spam Sms"
+time.sleep(1)
+print "[12] Install Bajingan v6" #Username: BAJINGAN | Password: Gans
+time.sleep(1)
+print "[13] Tools Bajingan v2"
+time.sleep(1)
+print "[14] Santet Online"
+time.sleep(1)
+print "[15] Nuyul Pulsa"
+time.sleep(1)
+print "[16] Script Lacak Lokasi Orang"
+time.sleep(1)
+print "[17] Cyber Trojan Indonesia"
+time.sleep(1)
+print "[18] LiteSpam"
+time.sleep(1)
+print "[19] Sadap Whatsapp"
+time.sleep(1)
+print "[20] Bobol Wifi"
+time.sleep(1)
+print "[21] Ddos Attack Trojan"
+time.sleep(1)
+print "[22] Ddos Attack via Termux"
+time.sleep(1)
+print "[23] Chat Subhan Fadilah"
+time.sleep(1)
+print "[24] Install Bahan-bahan"
+time.sleep(1)
+print "=" * 25
+print
+pilih = raw_input('[?] Masukkan Nomer Pilihan Anda ==> ')
+if pilih == '1':
+	os.system('clear')
+	os.system('git clone https://github.com/cyweb/hammer')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Hammer >> Masuk ke Directory Hammer >> ketik chmod +x hammer.py >> untuk menjalankan ketik python3 hammer.py >> untuk system kerja python3 hammer.py -s [ip target] -p [80] -t [135]'.center(10))
+	time.sleep(5)
+elif pilih == '2':
+	os.system('clear')
+	os.system('git clone https://github.com/CyborgSpLiTTer/ServerDDos')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Server-Ddos-Website >> Masuk ke Directory Server-Ddos-Website (cd ServerDDos) >> untuk menjalankan ketik python3 DDos.py'.center(10))
+	time.sleep(5)
+elif pilih == '3':
+	os.system('clear')
+	os.system('git clone https://github.com/kancotdiq/ipcs')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Script Mr.Rffr >> Masuk ke Directory Script Mr.Rffr (cd ipcs) >> untuk menjalankan ketik python ipcs.py'.center(10))
+	time.sleep(5)
+elif pilih == '4':
+	os.system('clear')
+#	os.system('git clone https://github.com/kancotdiq/ipcs')
+#	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Perkiraan Cuaca >> untuk menjalankan ketik $ curl http://wttr.in/ (lokasi)'.center(2))
+	time.sleep(5)
+elif pilih == '5':
+	os.system('clear')
+	os.system('git clone https://github.com/Cvar1984/LiteOTP.git')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Spam Sms JDJID >> Masuk ke Directory Spam Sms JDJIDr (cd LiteOTP) >> untuk menjalankan ketik php lite.php [no_target]'.center(10))
+	time.sleep(5)
+elif pilih == '6':
+	os.system('clear')
+#	os.system('git clone https://github.com/Cvar1984/LiteOTP.git')
+#	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Game Moon Buggy >> untuk menjalankan ketik $ moon-buggy'.center(2))
+	time.sleep(5)
+elif pilih == '7':
+	os.system('clear')
+	os.system('git clone https://github.com/pirmansx/mbf')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Hack Fb v1.0 >> Masuk ke Directory Hack Fb v1.0 (cd mbf) >> untuk menjalankan ketik python MBF.py'.center(10))
+	time.sleep(5)
+elif pilih == '8':
+	os.system('clear')
+	os.system('wget http://override.waper.co/files/facebook.apk')
+	os.system('wget http://override.waper.co/files/password.apk')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Hack Fb v1.2 >> ketik $ mv facebook.apk facebook.py  >> ketik $ mv password.apk password.txt >> untuk menjalankan ketik $ python facebook.py'.center(10))
+	time.sleep(5)
+elif pilih == '9':
+	os.system('clear')
+	os.system('git clone https://github.com/Ubaii/script-deface-creator')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Membuat Script Deface >> Masuk ke Directory Membuat Script Deface (cd script-deface-creator) >> ketik $ chmod +x create.py >> untuk menjalankan ketik $ python2 create.py'.center(10))
+	time.sleep(5)
+elif pilih == '10':
+	os.system('clear')
+	os.system('git clone https://github.com/CrazyError/CS')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Tools Crazy Sms >> Masuk ke Directory Tools Crazy Sms (cd CS) >> untuk menjalankan ketik $ sh CS.sh'.center(10))
+	time.sleep(5)
+elif pilih == '11':
+	os.system('clear')
+	os.system('git clone https://github.com/Rusmana-ID/rus')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Tools Rusmana Spam Sms >> Masuk ke Directory Rusmana Spam Sms (cd rus) >> untuk menjalankan ketik $ sh rus.sh'.center(10))
+	time.sleep(5)
+elif pilih == '12':
+	os.system('clear')
+	os.system('git clone https://github.com/DarknessCyberTeam/BAJINGANv6')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Install Bajingan v6 >> Masuk ke Directory Install Bajingan v6 (cd BAJINGANv6) >> untuk menjalankan ketik $ sh BAJINGAN.sh >> Apabila membutuhkan Username dan Password == '.center(10))
+	jalan ('Username: BAJINGAN | Password: Gans'.center(10))
+	time.sleep(5)
+elif pilih == '13':
+	os.system('clear')
+	os.system('git clone https://github.com/DarknessCyberTeam/B4J1N64Nv4')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Tools Tools Bajingan v2 >> Masuk ke Directory Tools Bajingan v2 (cd B4J1N64Nv4) >> untuk menjalankan ketik $ sh bajingan.sh'.center(10))
+	time.sleep(5)
+elif pilih == '14':
+	os.system('clear')
+	os.system('git clone https://github.com/Gameye98/santet-online')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Tools Santet Online >> Masuk ke Directory Santet Online (cd santet-online) >> untuk menjalankan ketik $ python2 santet.py'.center(10))
+	time.sleep(5)
+elif pilih == '15':
+	os.system('clear')
+	os.system('git clone https://github.com/mrcrack99/pulsa')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Tools Nuyul Pulsa >> Masuk ke Directory Nuyul Pulsa (cd pulsa) >> untuk menjalankan ketik $  sh pulsa.sh'.center(10))
+	time.sleep(5)
+elif pilih == '16':
+	os.system('clear')
+	os.system('git clone https://github.com/thelinuxchoice/locator')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Script Lacak Lokasi Orang >> Masuk ke Directory Script Lacak Lokasi Orang (cd locator) >> untuk menjalankan ketik $  bash locator.sh'.center(10))
+	time.sleep(5)
+elif pilih == '17':
+	os.system('clear')
+	os.system('git clone https://github.com/CyberTrojanIndonesia/DARKDDOS')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Cyber Trojan Indonesia >> Masuk ke Directory Cyber Trojan Indonesia (cd DARKDDOS) >> untuk menjalankan ketik $  sh DARKDDOS.sh'.center(10))
+	time.sleep(5)
+elif pilih == '18':
+	os.system('clear')
+	os.system('git clone https://github.com/4L13199/LITESPAM')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools LiteSpam >> Masuk ke Directory LiteSpam (cd LITESPAM) >> untuk menjalankan ketik $ bash LITESPAM.sh'.center(10))
+	time.sleep(5)
+elif pilih == '19':
+	os.system('clear')
+	os.system('git clone https://github.com/AndriGanz/whatshack')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Sadap Whatsapp >> Masuk ke Directory Sadap Whatsapp (cd whatshack) >> untuk menjalankan ketik $ sh whatshack.sh'.center(10))
+	time.sleep(5)
+elif pilih == '20':
+	os.system('clear')
+	os.system('git clone https://github.com/esc0rtd3w/wifi-hacker')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Bobol Wifi >> Masuk ke Directory Bobol Wifi (cd wifi-hacker/) >> ketik perintah $ chmod +x wifi-hacker.sh >> untuk menjalankan ketik $ ./wifi-hacker.sh'.center(10))
+	time.sleep(5)
+elif pilih == '21':
+	os.system('clear')
+	os.system('git clone https://github.com/BPCATTACKER/DOS-TOOKITS')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Ddos Attack Trojan >> Masuk ke Directory Ddos Attack Trojan (cd DOS-TOOKITS) >> untuk menjalankan ketik $ sh DoS-TOOKIT.sh'.center(10))
+	time.sleep(5)
+elif pilih == '22':
+	os.system('clear')
+	os.system('git clone https://github.com/cyweb/hammer')
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Cara Menggunakan tools Hammer >> Masuk ke Directory Hammer >> ketik chmod +x hammer.py >> untuk menjalankan ketik python3 hammer.py >> untuk system kerja python3 hammer.py -s [ip target] -p [80] -t [135]'.center(10))
+	time.sleep(5)
+elif pilih == '23':
+	os.system('clear')
+#	os.system('git clone https://github.com/cyweb/hammer')
+#	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Maaf Untuk point "Chat Subhan Fadilah" sedang di Perbaiki :)'.center(10))
+	time.sleep(5)
+elif pilih == "24":
+	os.system('clear')
+	os.system('pkg update && pkg upgrade')
+	os.system('pkg install bash')
+	os.system('pkg install python2')
+	os.system('pkg install python3')
+	os.system('pkg install lolcat')
+	os.system('pkg install figlet')
+	os.system('pkg install toilet')
+	os.system('pkg install pip')
+	os.system('pip2 install requirements')
+	os.system('pkg install gem')
+	os.system('pkg install php')
+	os.system('pkg install neofetch')
+	os.system('pkg install git')
+	os.system('pip2 install requests')
+	print
+	time.sleep(5)
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan('[*] Penginstalan Bahan-bahan sudah full boss. :). Silahkan langsung anda clone tools yang anda Butuhkan boss.. :)'.center(10))
+	time.sleep(5)
+else:
+	os.system('clear')
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Nomer Yang Anjeun Masukkan Tidak Ada Dalam Daftar Tools Ini Boss :( [*]'.center(2))
+	time.sleep(5)
+	print
+os.system('clear')
+pesan = raw_input('[?] Apakah Anda ingin mengulang (y/t) ==> ')
+if pesan == 'y':
+	time.sleep(3)
+	os.system('python sumandingtools.py')
+elif pesan == 't':
+	time.sleep(3)
+	os.system('clear')
+	exit()
+else:
+	time.sleep(3)
+	def jalan(kata):
+		for c in kata:
+			sys.stdout.write(c)
+			sys.stdout.flush()
+			time.sleep(0.1)
+	jalan ('[*] Kamu mah Nulisna Ngacaprak Waee boss :( [*]'.center(10))
+	os.system('clear')
